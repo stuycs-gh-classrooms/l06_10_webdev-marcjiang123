dcec21f663084e16b4eb6bdbfe8117ae74f4afdd
Marc Jiang <75910947+marcjiang123@users.noreply.github.com>
2022-06-01 15:20:35 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,47 @@
-function setup() {
-  // put setup code here
+var cx;
+var cy;
+var theta;
+var radius;
+var centerX;
+var centerY;
+var cColor;
+var c;
+
+function setup(){
+  createCanvas(400,400);
+  radius = 100;
+  centerX = width/2;
+  centerY = height/2;
+  theta = 0.;
+  cColor = 0.;
+  c = 0;
+  fill(0,0,0);
 }
 
-function draw() {
-  // put drawing code here
+function draw(){
+  background(255);
+  cx = newX(radius,centerX,theta);
+  cy = newY(radius,centerY,theta);
+  cColor++;
+  if (cColor % 60 == 0){
+    c+=10;
+    fill(0,c,0);
+  }
+  if (c > 255){
+    c = 0;
+    fill(0,c,0);
+  }
+    
+  circle(cx,cy,radius);
+  line(centerX,centerY,cx,cy);
+  theta++;
+  
+}
+ 
+function newX(r,x,th){
+  return cx = cos(radians(th)) * r + x;
+}
+
+function newY(int r,int y,float th){
+  return cy = sin(radians(th)) * r + y;
 }
